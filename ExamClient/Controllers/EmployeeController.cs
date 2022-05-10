6eb38d5302d2cab226ac1bb9b0307c4e5f11b641
nmq126 <minhquangngo.98@gmail.com>
2022-05-10 22:06:18 +0700

    push

---
+++ ExamClient/Controllers/EmployeeController.cs
@@ -0,0 +1,97 @@
+﻿using ExamClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamClient.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private Service1Client service = new Service1Client();
+        // GET: Employee
+        public ActionResult Index(string keyword)
+        {
+            ViewBag.Keyword = keyword;
+            if (!String.IsNullOrEmpty(keyword))
+            {
+                var list = service.FindByDepartment(keyword);
+                return View(list);
+            }
+
+            return View(service.GetAll());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                service.Save(employee);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ExamClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamClient.MvcApplication" Language="C#" %>
